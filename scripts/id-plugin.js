3f1736e58ed9aa784c07b1ed0a3e508be10ad0f1
boaromayo <nlypoblet@gmail.com>
2017-11-12 21:57:24 -0800

    Removed js file; added id-plugin directly to entry layout

--- scripts/id-plugin.js
+++
@@ -1,8 +0,0 @@
-/*==============================================================================
- * id-plugin.js
- *  @desc This plugin allows the intensedebate comment system on website.
- *==============================================================================*/
-
-var idcomments_acct = '24ffbf65f82f6773f0c15e6c95541c40';
-var idcomments_post_id;
-var idcomments_post_url;
