05cc925d03eff45a840434784307a22b59eb26fa
boaromayo <nlypoblet@gmail.com>
2017-11-12 20:56:59 -0800

    Add intensedebate plugin for test

---
+++ scripts/id-plugin.js
@@ -0,0 +1,8 @@
+/*==============================================================================
+ * id-plugin.js
+ *  @desc This plugin allows the intensedebate comment system on website.
+ *==============================================================================*/
+
+var idcomments_acct = '24ffbf65f82f6773f0c15e6c95541c40';
+var idcomments_post_id;
+var idcomments_post_url;
