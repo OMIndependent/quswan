bf9b7865d05eda65647866b0ddd40c7be586d652
boaromayo <nlypoblet@gmail.com>
2018-04-10 10:08:12 -0700

    Updated index generator to (mostly) JavaScript standard style

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
 #!/usr/bin/env node
+// This file generates the static website using Metalsmith plugins.
 
 // Do not use any unneeded vars when running
-'use strict';
+'use strict'
 
 // Import plugins
-const pkg           = require('./package.json');
+const pkg             = require('./package.json')
 
-const Metalsmith    = require('metalsmith');
-const publish       = require('metalsmith-publish');
-const drafts        = require('metalsmith-drafts');
-const collections   = require('metalsmith-collections');
-const images        = require('metalsmith-scan-images');
-const assets        = require('metalsmith-assets');
-const pug           = require('metalsmith-pug');
-const markdown      = require('metalsmith-markdown');
-const emoji         = require('metalsmith-emoji');
-const moment        = require('metalsmith-moment');
-const layouts       = require('metalsmith-layouts');
-const permalinks    = require('metalsmith-permalinks');
-const pagination    = require('metalsmith-pagination');
-const wordcount     = require('metalsmith-word-count');
-const htmlmin       = require('metalsmith-html-minifier');
-const cssmin        = require('metalsmith-clean-css');
-const imgmin        = require('metalsmith-imagemin');
-const sitemap       = require('metalsmith-mapsite');
-const browsersync   = require('metalsmith-browser-sync');
+const Metalsmith      = require('metalsmith')
+const publish         = require('metalsmith-publish')
+const drafts          = require('metalsmith-drafts')
+const collections     = require('metalsmith-collections')
+const images          = require('metalsmith-scan-images')
+const assets          = require('metalsmith-assets')
+const pug             = require('metalsmith-pug')
+const markdown        = require('metalsmith-markdown')
+const emoji           = require('metalsmith-emoji')
+const moment          = require('metalsmith-moment')
+const layouts         = require('metalsmith-layouts')
+const permalinks      = require('metalsmith-permalinks')
+const pagination      = require('metalsmith-pagination')
+const wordcount       = require('metalsmith-word-count')
+const htmlmin         = require('metalsmith-html-minifier')
+const cssmin          = require('metalsmith-clean-css')
+const imgmin          = require('metalsmith-imagemin')
+const sitemap         = require('metalsmith-mapsite')
+const browsersync     = require('metalsmith-browser-sync')
 
 /* Global settings */
-const desc = "The Socially Aware Magic Swordsman's independent blog site generated with Metalsmith";
-const numPosts = 10;
+const desc = 'The Socially Aware Magic Swordsman\'s independent blog site generated with Metalsmith'
+const numPosts = 10
 const entryPattern = ['posts/*.md', 'liveblogs/**/*.md', '!liveblogs/**/index.*',
-  '!liveblogs/**/masterlist.*', 'trivia/**/*'];
-var destDir = './build/';
+  '!liveblogs/**/masterlist.*', 'trivia/**/*']
+var destDir = './build/'
 
 /* Global metadata */
 var meta = {
   site: {
-    title: "Quesada's Swan | ",
-    url: "http://quswan.net/"
+    title: 'Quesada\'s Swan | ',
+    url: 'http://quswan.net/'
   },
-  name: "Quesada's Swan",
+  name: 'Quesada\'s Swan',
   description: desc,
-  generator: "Metalsmith",
-  generatorurl: "http://metalsmith.io/",
-  version:  pkg.version
-};
+  generator: 'Metalsmith',
+  generatorurl: 'http://metalsmith.io/',
+  version: pkg.version
+}
 
 /* Directory paths */
 var dir = {
-  base:   __dirname,
-  source:    './src/',
-  dest:  destDir
-};
+  base: __dirname,
+  source: './src/',
+  dest: destDir
+}
 
 /* Collections metadata list */
 var collexions = {
@@ -81,20 +82,20 @@ var collexions = {
       layout: 'page.pug'
     }
   }
-};
+}
 
 /* Pug plugin settings */
 var opts = {
   pretty: false,
   useMetadata: true
-};
+}
 
 /* Emoji settings */
 var emset = {
   pattern: ['**/*.md', '**/**/*.md', '**/*.html', '**/**/*.html'],
   convertToImages: true,
   processShortnames: true
-};
+}
 
 /* Moment time format settings */
 /* NOTE: 'date' is taken from markdown file's YAML front matter.
@@ -103,14 +104,14 @@ Input date formats accepted are:
   "2020-07-01 15:03:25 -0700" or "2020-07-01 09-0700"
 The output date format returns:
   "dddd, MMMM Do, YYYY" -> "X-day, July 1st, 2020" */
-/* 'broadcastDate' is also used for TV shows released on 
-  different days. */
-var mtime = ['date'];
+/* 'broadcastDate' is also used for TV shows released on
+different days. */
+var mtime = ['date']
 
 /* Permalink settings */
 var perm = {
   relative: false
-};
+}
 
 /* Pagination settings */
 var pagi = {
@@ -121,47 +122,47 @@ var pagi = {
     noPageOne: true,
     path: 'page/:num/index.html'
   }
-};
+}
 
 /* Post publish settings */
 var publishOpts = {
   draft: true,
   private: true,
   unlisted: true
-};
+}
 
 /* Template/layout engine plugin settings */
 var configTemplate = {
   engine: 'pug',
   directory: 'layouts',
   default: 'layout.pug'
-};
+}
 
 /* Image gallery parsing */
-var imgPattern = 'src/**/**/gallery.*';
+var imgPattern = 'src/**/**/gallery.*'
 
 /* Assets settings */
 var assetsOpts = {
   source: './assets',
   destination: './assets'
-};
+}
 
 // Clean build directory or not?
-var clean = true;
+var clean = true
 
 // Output word count. When false, use readingTime instead.
-var word = false;
+var word = false
 
 /* HTML minifier settings */
 var minify = {
   collapseBooleanAttributes: false,
   collapseWhitespace: false
-};
+}
 
 /* CSS minifier settings */
 var cssminify = {
   files: ['assets/*.css', 'assets/**/*.css']
-};
+}
 
 /* Image minifier settings */
 var imgminify = {
@@ -169,13 +170,13 @@ var imgminify = {
   svgoPlugins: [{
     removeViewBox: false
   }]
-};
+}
 
-// Toggle debug log
-var log = true;
+// Toggle debug
+var log = false
 
 Metalsmith(dir.base)
-  .clean(clean)  // Clean the build directory
+  .clean(clean) // Clean the build directory
   .metadata(meta) // Get metadata
   .source(dir.source) // Get source files from './src/' directory
   .destination(dir.dest) // Place final web files into destination directory
@@ -209,47 +210,45 @@ Metalsmith(dir.base)
   .use(assets(assetsOpts)) // Add assets to site
 
   // Minify/compress files
-  .use(htmlmin('*.html',minify))
+  .use(htmlmin('*.html', minify))
   .use(cssmin(cssminify))
   .use(imgmin(imgminify))
 
   .use(sitemap(meta.site.url)) // Print xml file of sitemap for search engines
-  
+
   // Debug and print any errors in console
-  //.use(debug(log))
+  .use(debug(log))
   .use(browsersync({
     server: destDir,
-    files:  ['./src/**/**/*', './src/**/*', './src/*',
+    files: ['./src/**/**/*', './src/**/*', './src/*',
       'assets/*'], // Note changes in these files
     port: 8080,
     injectChanges: false,
     codeSync: false
-  }), function(err) {
-    if (err) { throw err; }
+  }), function (err) {
+    if (err) { throw err }
   })
 
   // Build site and call exceptions when things go wrong
-  .build(function(err) {
-    if (err) { throw err; }
-    else { console.log("Build complete.\n"); }
-  });
-
+  .build(function (err) {
+    if (err) { throw err } else { console.log('Build complete.\n') }
+  })
 
 // Debug function to check if website loads correctly
 function debug(log) {
   return function(files, Metalsmith, done) {
     if (log) {
-      console.log("\nMETADATA:");
-      console.log(Metalsmith.metadata());
+      console.log('\nMETADATA:')
+      console.log(Metalsmith.metadata())
 
       for (var f in files) {
-        console.log("\nFILE:");
-        console.log(files[f]);
+        console.log('\nFILE:')
+        console.log(files[f])
       }
 
-      console.log("\nCOLLECTIONS:");
-      console.log(Metalsmith.metadata().collections);
+      console.log('\nCOLLECTIONS:')
+      console.log(Metalsmith.metadata().collections)
     }
-    done();
-  };
+    done()
+  }
 }
